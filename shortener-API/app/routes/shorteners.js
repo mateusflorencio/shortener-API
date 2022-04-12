953cdf6039205f2529953c9fb8f0f6699f9155a2
Mateus Florêncio <mateuusflorencio@outlook.com.br>
2022-04-12 16:35:51 -0300

    atualizando url

--- shortener-API/app/routes/shorteners.js
+++ shortener-API/app/routes/shorteners.js
@@ -4,32 +4,6 @@ const WithAuth = require("../middlewares/auth");
 const Short = require("../model/shortener");
 const generateCode = require("../middlewares/code")
 
-
-router.post("/new", WithAuth, async (req, res) => {
-    const {
-        url
-    } = req.body;
-
-    try {
-        let urlshort = generateCode();
-        let dateUrl = new Short({
-            url: url,
-            codUrl: urlshort,
-            author: req.user._id
-        });
-        await dateUrl.save();
-        res.json(dateUrl);
-
-    } catch (err) {
-        res.status(500).json({
-            err: `${err}`
-        })
-    }
-
-
-
-});
-
 router.get("/", WithAuth, async (req, res) => {
     try {
         let urls = await Short.find({
@@ -59,7 +33,74 @@ router.get("/:code", async (req, res) => {
             error: "not found"
         })
     }
-})
+});
+
+router.post("/new", WithAuth, async (req, res) => {
+    const {
+        url
+    } = req.body;
+
+    try {
+        let urlshort = generateCode();
+        let dateUrl = new Short({
+            url: url,
+            codUrl: urlshort,
+            author: req.user._id
+        });
+        await dateUrl.save();
+        res.json(dateUrl);
+
+    } catch (err) {
+        res.status(500).json({
+            err: `${err}`
+        })
+    }
+
+
+
+});
+
+router.put(("/:codUrl"), WithAuth, async (req, res) => {
+    const url = req.body.url;
+    const codUrl = req.params.codUrl;
+
+    try {
+        let shortener = await Short.findOne({
+            codUrl: codUrl
+        });
+        if (isOwner(req.user, shortener)) {
+            let shortener = await Short.findOneAndUpdate({
+                codUrl: codUrl
+            }, {
+                $set: {
+                    url: url
+                }
+            }, {
+                upsert: true,
+                "new": true
+            });
+            res.json(shortener);
+        } else {
+            res.status(403).json({
+                error: "Permission denied"
+            });
+        };
+    } catch (error) {
+        res.status(500).json({
+            error: `"Problem to update ${error}`
+        });
+    };
+});
+
+function isOwner(user, shortener) {
+    if (JSON.stringify(user._id) == JSON.stringify(shortener.author))
+
+        return true;
+    else {
+        return false;
+    };
+};
+
 
 
 module.exports = router;
