c11f82f8fb5abb1a71b548fa3bc75e22e388a85a
Mateus Florêncio <mateuusflorencio@outlook.com.br>
2022-04-14 15:38:28 -0300

    arrumando cors

--- shortener-API/app.js
+++ shortener-API/app.js
@@ -10,14 +10,16 @@ const shortenerRouter = require('./app/routes/shorteners');
 const homeRouter = require('./app/routes/home');
 
 const app = express();
-app.use(cors());
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({
   extended: false
 }));
+app.use(cors());
+
 
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(cors());
 
 
 //routs
